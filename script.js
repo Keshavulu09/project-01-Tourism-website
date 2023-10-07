7e05ac1acb8d4e4d77a82fcca4d03b80122fcefb
Prasad81061 <128883346+Prasad81061@users.noreply.github.com>
2023-10-07 12:36:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-Container');
 
-
-searchBtn.addEventListener('click', () =>{
-    searchBtn.classList.toggle('fa-times');
-    searchBar.classList.toggle('active');
-});
